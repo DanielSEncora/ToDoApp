6104ed71fadaf9dd7bfebb419cf68789e5ae9e52
Daniel Salas <daniel.salas@ENCEDCHMOLT0965.local>
2025-01-21 23:30:47 -0700

    Fixed bug with the globalexcptionhandler class, a parameter was not right, therefore the override annotation was not working as intended.

--- Back-end/encora/src/main/java/com/encora/exception/GlobalExceptionHandler.java
+++ Back-end/encora/src/main/java/com/encora/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -47,10 +48,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // Handle MethodArgumentNotValidException (validation errors)
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
